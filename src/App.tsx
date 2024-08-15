7ebbb2cd98f65a89b00f3960588d983629ec2d42
Sawom <asawom250@gmail.com>
2024-08-15 08:41:13 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1> react with redux </h1>
+
+      <div>
+        
+      </div>
+
+    </>
+  );
+}
+
+export default App;
